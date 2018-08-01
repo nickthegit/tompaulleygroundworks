7bbcbeb8a7fbfa4434c73f78426e1dc071ae5c61
nickthegit <nick@rosielee.co.uk>
2018-08-01 16:45:54 +0100

    https images for test

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -6,19 +6,19 @@
             <!-- Slides -->
             <div class="swiper-slide">
                 <picture>
-                    <source media="(min-width: 1024px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1800/v1533128578/tpgw/tpgw1.jpg 2x">
-                    <source media="(min-width: 769px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1800/v1533128578/tpgw/tpgw1.jpg 2x">
-                    <source media="(min-width: 481px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_770/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1440/v1533128578/tpgw/tpgw1.jpg 2x">
-                    <source media="(min-width: 321px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_480/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_960/v1533128578/tpgw/tpgw1.jpg 2x">
-                    <source media="(min-width: 200px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_320/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_640/v1533128578/tpgw/tpgw1.jpg 2x">
-                    <img src="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg" alt="">
+                    <source media="(min-width: 1024px)" srcset="https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg, https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1800/v1533128578/tpgw/tpgw1.jpg 2x">
+                    <source media="(min-width: 769px)" srcset="https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg, https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1800/v1533128578/tpgw/tpgw1.jpg 2x">
+                    <source media="(min-width: 481px)" srcset="https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_770/v1533128578/tpgw/tpgw1.jpg, https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1440/v1533128578/tpgw/tpgw1.jpg 2x">
+                    <source media="(min-width: 321px)" srcset="https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_480/v1533128578/tpgw/tpgw1.jpg, https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_960/v1533128578/tpgw/tpgw1.jpg 2x">
+                    <source media="(min-width: 200px)" srcset="https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_320/v1533128578/tpgw/tpgw1.jpg, https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_640/v1533128578/tpgw/tpgw1.jpg 2x">
+                    <img src="https://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg" alt="">
                 </picture>  
             </div>
             <div class="swiper-slide">
-                <img src="http://via.placeholder.com/1920x1080" alt="">
+                <img src="https://via.placeholder.com/1920x1080" alt="">
             </div>
             <div class="swiper-slide">
-                <img src="http://via.placeholder.com/1920x1080" alt="">
+                <img src="https://via.placeholder.com/1920x1080" alt="">
             </div>
 
         </div>
