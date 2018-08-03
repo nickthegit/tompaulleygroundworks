c84168586dfeadeac4a723468479f55513ace9f7
nickthegit <nick@rosielee.co.uk>
2018-08-03 14:18:14 +0100

    got it working

---
+++ components/responsiveimage.vue
@@ -0,0 +1,28 @@
+<template>
+    <picture>
+        <source media="(min-width: 1024px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1800/v1533128578/tpgw/tpgw1.jpg 2x">
+        <source media="(min-width: 769px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1800/v1533128578/tpgw/tpgw1.jpg 2x">
+        <source media="(min-width: 481px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_770/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_1440/v1533128578/tpgw/tpgw1.jpg 2x">
+        <source media="(min-width: 321px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_480/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_960/v1533128578/tpgw/tpgw1.jpg 2x">
+        <source media="(min-width: 200px)" srcset="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_320/v1533128578/tpgw/tpgw1.jpg, http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_640/v1533128578/tpgw/tpgw1.jpg 2x">
+        <img src="http://res.cloudinary.com/jonserness/image/upload/ar_1.777777,c_fill,q_auto:good,w_900/v1533128578/tpgw/tpgw1.jpg" alt="">
+    </picture> 
+</template>
+
+<script>
+
+export default {
+    props: {
+        imageObj: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    img {
+        width: 100%;
+    }
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,112 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        Tom Paulley Groundworks
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div id="app">
+
+    <navigation/>
+
+    <div id="fullpage">
+      <section class="section hero" data-anchor="home" :style="{ 'background-image': 'url(' + hero.image + ')' }">
+        <div class="home-wrapper">
+          <img src="https://res.cloudinary.com/jonserness/image/upload/c_fill,q_auto:good,w_650/v1533210167/tpgw/tom-paulley-groundworks-logo.png" alt="Tom Paulley Groundworks Logo">
+          <h1>{{ hero.strapline }}</h1>
+        </div>
+      </section>
+
+      <section class="section services" data-anchor="services">
+        <services/> 
+      </section>
+
+      <section class="section" data-anchor="contact">
+        <contact/>
+      </section>
+
     </div>
-  </section>
+
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import navigation from '~/components/navigation.vue'
+import services from '~/components/services.vue'
+import contact from '~/components/contact.vue'
+
+import TweenMax from 'gsap'
 
 export default {
+  name: 'app',
   components: {
-    AppLogo
+    navigation,
+    services,
+    contact
+  },
+  data() {
+    return {
+      hero: {
+        image: 'https://res.cloudinary.com/jonserness/image/upload/c_fill,q_auto,w_1920,h_1080/v1533204731/tpgw/home-page.jpg',
+        strapline: 'Tom Paulley Groundworks is a Dorset based ground works company offering services towards all aspects of groundworks, from excavations to drainage and surfacing to landscaping, serving all neighbouring counties.'
+      }
+    }
+  },
+  mounted () {
+    if (process.browser) {
+      var fullpage = require('fullpage.js');
+      var servicesOnce = 'on';
+      new fullpage('#fullpage', {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        menu: '#navMenu',
+        onLeave: function(origin, destination, direction){
+          if(destination.anchor == "services" && direction =='down' && servicesOnce == 'on'){
+                const servTl = new TimelineLite({onComplete: function() { servicesOnce = 'off' }}).delay(0.75);
+                servTl.fromTo('.services__grid-wrapper article', 0.5, {y: -50, autoAlpha: 0}, {y: 0, autoAlpha: 1}) 
+                .fromTo('.services__grid-wrapper aside', 0.75, {y: 50, autoAlpha: 0}, {y: 0, autoAlpha: 1}, "-=0.1");        
+          }
+        }
+      })
+      const tl = new TimelineLite();
+      tl.fromTo('.hero', 0.75, {autoAlpha: 0},{autoAlpha:1}, "start")
+        .fromTo('.home-wrapper img', 0.3, {y: -50, autoAlpha: 0}, {y: 0, autoAlpha: 1}, "+=0.2")
+        .fromTo('.home-wrapper h1', 0.3, {y: 50, autoAlpha: 0}, {y: 0, autoAlpha: 1}, "+=0.1")
+        .fromTo('header', 0.3, {y: "-100%", autoAlpha: 0}, {y: "0%", autoAlpha: 1}, "-=0");
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+  @import "~/assets/scss/base/_variables.scss";
+  @import "~/assets/scss/base/_reset.scss";
+  @import "~/assets/scss/elements/_structure.scss";
+  @import "~/assets/scss/base/_mediaquery.scss";
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .hero {
+    position: relative;
+    .home-wrapper {
+      background: rgba($black,0.3);
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 40px;
+      box-sizing: border-box;
+    }
+    h1 {
+        font-size: 3.4vh;
+        line-height: 3.6vh;
+        width: 100%;
+        max-width: 800px;
+        padding-top: 40px;
+    }
+    img {
+      height: 32%;
+      width: auto;
+    }
+    @include breakpoint(mobile) { 
+      img {
+        width: 50%;
+        height: auto;
+      }
+    }
+  }
 </style>
+
 
