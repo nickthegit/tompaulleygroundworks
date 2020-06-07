8f32b2265c205d3e38a518bcf3e7a6da646db09f
nickthegit <nick@rosielee.co.uk>
2020-06-07 17:21:58 +0100

    services slug images start

--- pages/services/_slug.vue
+++ pages/services/_slug.vue
@@ -8,13 +8,32 @@
         <li v-for="(item, itemIndex) in service.list" :key="`${itemIndex}-listitem`">{{ item }}</li>
         </ul>
       </p>
+      <section class="img-container" v-for="(imgSection, sectionIndex) in service.image_sections" :key="`${sectionIndex}-imgsection`" >
+        <div v-if="imgSection.type === 'slider'" class="slider"><h1>SLIDER</h1></div>
+        <div v-else :class="[imgSection.type, `length-${imgSection.images.length}`, `${imgSection.images.length % 2 ? 'odd' : 'even'}`]" >
+          <div class="img-wrap" v-for="(image, imgIndex) in imgSection.images" :key="`${imgIndex}-imgof-${sectionIndex}`">
+            <cloudinary-image
+              :image="image"
+              :width="800"
+              :height="600"
+              size="full"
+              :alt="`An image about ${service.title} by Tom Paulley Groundworks`"
+              folder="tpgw"
+            />
+          </div>
+        </div>
+      </section>
     </article>
     <nuxt-link class="back-to-services" to="/services">← Back to services</nuxt-link>
   </main>
 </template>
 
 <script>
+ import cloudinaryImage from '~/components/cloudinaryImage.vue'
   export default {
+        components: {
+      cloudinaryImage
+    },
     async asyncData({ $content, params }) {
       const service = await $content('services', params.slug).fetch()
       return {
@@ -89,6 +108,34 @@
     display: block;
     &:hover {
       color: $primary;
+    }
+  }
+  .img-container {
+    width: 100%;
+    position: relative;
+    padding: 40px 0;
+  }
+  .gallery {
+    width: 100%;
+    position: relative;
+    display: grid;
+    grid-template: auto / 1fr 1fr;
+    grid-auto-flow: dense;
+    .img-wrap:last-child:nth-last-child(odd) {
+      grid-column: auto / span 2;
+    }
+  }
+  .img-wrap {
+    width: 100%;
+    position: relative;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 66.666666%;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      transition: transform 0.25s ease-out;
     }
   }
 </style>
